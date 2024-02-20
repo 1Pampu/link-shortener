d3d3c67d4c6901724a35b76536113fcbe26355c8
1Pampu <piampianimartin@gmail.com>
2024-02-20 11:41:33 -0300

    fix names :)

--- links/forms.py
+++ links/forms.py
@@ -9,7 +9,7 @@ class LinkForm(forms.ModelForm):
 
     original = forms.CharField(label='Paste the URL here:',
                                widget=forms.TextInput(attrs={'placeholder': 'https://'}))
-    short = forms.CharField(label='https://shortly.com/li/', label_suffix=" ",
+    short = forms.CharField(label='https://shortly-mp.onrender.com/li/', label_suffix=" ",
                             widget=forms.TextInput(attrs={'placeholder': 'Custom link'}),
                             required=False)
 
